1b74e0ab3c388305e46ce036dadc5ca2e183cf8d
Nikhil <nikhil.shah2008@gmail.com>
2022-08-23 01:57:26 +0530

    Day 6

--- Day 5/Part 2 - Static/bankingDLL/bankingDLL/Checking.cs
+++ Day 5/Part 2 - Static/bankingDLL/bankingDLL/Checking.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace bankingDLL
 {
-    public  class Checking : Accounts
+    public sealed  class Checking : Accounts
     {
         static int _autoNumChecking = 700;
         public Checking( string p_accName, string p_accLastName, double p_accBalance, int p_accPin) : base( p_accName, p_accLastName, p_accBalance, p_accPin)
