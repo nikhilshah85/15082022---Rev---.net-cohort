41970aa2f858ab55b612cb9ef9461eca8975c270
Nikhil <nikhil.shah2008@gmail.com>
2022-09-11 21:19:26 +0530

    Demo in day3

---
+++ Day 3/demo/Migrations/20220911144729_initial_new.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace demo.Migrations
+{
+    public partial class initial_new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
