945f334ec5702fa93d7bebe988701b530da015eb
Nikhil <nikhil.shah2008@gmail.com>
2022-09-16 01:02:52 +0530

    fetch n cors

--- day 14 - webapi/myShoppingAPI/myShoppingAPI/Program.cs
+++ day 14 - webapi/myShoppingAPI/myShoppingAPI/Program.cs
@@ -3,6 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddCors(policy =>
+{
+    policy.AddDefaultPolicy(rules =>
+    {
+        rules.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
