8567d7acb22a0d53c8dceabba4b70d7c01d8e4ce
Nikhil <nikhil.shah2008@gmail.com>
2022-09-27 20:40:27 +0530

    day 25 webapi

---
+++ day 25 - HttpClient - CORS/shoppingWebAPI/shoppingWebAPI/Model/EF/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shoppingWebAPI.Model.EF
+{
+    public partial class Product
+    {
+        public int PId { get; set; }
+        public string? PName { get; set; }
+        public string? PCategory { get; set; }
+        public int? PPrice { get; set; }
+        public bool? PisInStock { get; set; }
+    }
+}
---
+++ day 25 - HttpClient - CORS/shoppingWebAPI/shoppingWebAPI/Model/EF/shoppingDBContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace shoppingWebAPI.Model.EF
+{
+    public partial class shoppingDBContext : DbContext
+    {
+        public shoppingDBContext()
+        {
+        }
+
+        public shoppingDBContext(DbContextOptions<shoppingDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:trainer-server.database.windows.net,1433;Initial Catalog=shoppingDB;Persist Security Info=False;User ID=trainer;Password=Password@123465;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.PId)
+                    .HasName("PK__product__DD36D562AE7FBDF2");
+
+                entity.ToTable("product");
+
+                entity.Property(e => e.PId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("pId");
+
+                entity.Property(e => e.PCategory)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("pCategory");
+
+                entity.Property(e => e.PName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("pName");
+
+                entity.Property(e => e.PPrice).HasColumnName("pPrice");
+
+                entity.Property(e => e.PisInStock).HasColumnName("pisInStock");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
